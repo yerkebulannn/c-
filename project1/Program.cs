19ce9aae1cb2e0c4b0b5c58a3f3e205fe64a6db0
Yerkebulan <erkebulanlbk@gmail.com>
2024-03-19 19:41:51 +0500

    messages

---
+++ project1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace project1{
+    class Program{
+        static void Main(){
+            // float
+            // short
+            //byte
+            // int
+            // long
+            // double
+
+            // Console.ReadKey()
+
+        }
+    }
+}
